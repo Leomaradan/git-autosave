bb46fc42c535fb7e57ea495c1b72634c5fa5511a
Léo Maradan <3043485+Leomaradan@users.noreply.github.com>
2020-02-24 10:16:18 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# git-autosave
+# Git Auto-Save
 Perform auto-saving git repositories that correspond to some criteria
+
+# Installation
+__With npm:__
+
+```bash
+npm install -g git-autosave
+```
+
+With yarn 1.x:
+```bash
+yarn global add git-autosave
+```
+
+With yarn 2+ (installing and executing):
+```bash
+yarn dlx git-autosave
+```
+
+# How to use
+
+By default, the script will search for each git repository in the current folder, and in all subfolder, excluding node_modules folder
+
+In each folder, it will search for the `package.json` file, and check if a `.git` folder exists. If that criteria match, the script will check if the current branch has a keyword in its name (default keywork: `draft`)
+
+If all that critera match and there is some file to commit, it will add the files, commit with the date as a message, and push to origin.
+
+__The script doesn't check the last commit date. Executing the command multiple time can result of a lot of commits. Use this command in a schedule task__
+
+## Options
+Some options can be configured:
+
+* Base directory. The base directory can be set with `--dir=<my working dir>`
+* Search Term. The default search term is `package.json`. It can be override with `--search=<my entry point>`
+* Branch Term. The default branch term is `draft`. It can be override with `--branch=<my branch name>`
+* Searching through node_modules. By default, the script omit node_modules folder. It can be override with `-i`
---
+++ autosave.js
@@ -0,0 +1,94 @@
+const fs = require('fs');
+const path = require('path');
+const simpleGit = require('simple-git');
+const argv = require('argv');
+
+/**
+ * Recursive walk, and search for repos
+ * @param {string} dir - Base directory
+ * @param {{includeNodeModule: boolean; searchTerm: string; branchTerm: string}} options - Options for searching
+ */
+const walk = (dir, options) => {
+  const list = fs.readdirSync(dir);
+  list.forEach(filename => {
+    const file = path.resolve(dir, filename);
+    const stat = fs.statSync(file);
+    if (stat && stat.isDirectory()) {
+      /* Recurse into a subdirectory */
+      if (options.includeNodeModule || filename.indexOf('node_modules') === -1) {
+        walk(file, options);
+      }
+    } else {
+      if (filename.indexOf(options.searchTerm) !== -1) {
+        // Check if there is a git folder
+        const gitFolder = path.resolve(dir, '.git');
+
+        if (fs.existsSync(gitFolder)) {
+          const currentFolder = simpleGit(dir);
+          currentFolder.checkIsRepo((_, response) => {
+            if (response) {
+              checkAndCommit(currentFolder, options.branchTerm);
+            }
+          });
+        }
+      }
+    }
+  });
+};
+
+/**
+ * Perform git add, git commit and git push
+ * @param {*} git - SimpleGit handler
+ * @param {string} branchTerm - Keywork that must appear on the branch name to activate auto-saving
+ */
+const checkAndCommit = (git, branchTerm) => {
+  git.status((_, status) => {
+    const { files, current } = status;
+    if (current.indexOf(branchTerm) !== -1 && files.length > 0) {
+      console.log(`Auto-saving ${current}`);
+      git.add(files.map(file => file.path))
+        .commit(`Auto-saving (${(new Date()).toISOString()})`)
+        .push([], () => console.log('done'));
+    }
+  });
+}
+
+argv.option([
+  {
+    name: 'dir',
+    type: 'path',
+    description: 'Base path entry point',
+    example: "'auto-save.js --dir=\".\"'"
+  },
+  {
+    name: 'search',
+    type: 'string',
+    description: 'Search item to determine which folder is a git repo',
+    example: "'auto-save.js --search=package.lock'"
+  },
+  {
+    name: 'branch',
+    type: 'string',
+    description: 'keywork in a branch to determine if the branch must be auto-saved',
+    example: "'auto-save.js --branch=draft'"
+  },
+  {
+    name: 'include-node',
+    short: 'i',
+    type: 'boolean',
+    description: 'Include searching through node_modules folder. Can be very slow',
+    example: "'auto-save.js -i'"
+  }
+]);
+
+const resolve = argv.run();
+
+const defaultSearchTerm = 'package.json';
+const defaultBranchTerm = 'draft';
+
+const baseDir = resolve.options.dir !== undefined ? resolve.options.dir : path.resolve('.');
+const searchTerm = resolve.options.search !== undefined ? resolve.options.search : defaultSearchTerm;
+const branchTerm = resolve.options.branch !== undefined ? resolve.options.branch : defaultBranchTerm;
+const includeNodeModule = resolve.options['include-node'] !== undefined ? resolve.options['include-node'] : false;
+
+walk(baseDir, { searchTerm, branchTerm, includeNodeModule });
